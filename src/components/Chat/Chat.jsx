06b839c58ee331796beb7a0b563902f80628c4a7
sevor005 <sevor005@yandex.ru>
2018-12-23 16:33:07 +0700

    homework_2

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,14 +1,34 @@
-import Message from './Message'
+import React from 'react';
+import Message from '../Message';
 import './Chat.css';
 
 class Chat extends React.Component {
+  state = {
+    messageInput: '',
+    messages: []
+  }
+
+  sendMessageOnEnter = event => {
+    if(event.key !== 'Enter') return;
+    this.setState({ messages:[...this.state.messages, this.state.messageInput], messageInput: '' })
+  }
+
+  changeInputMessage = event => (event.target.value === '') ? null : this.setState({ messageInput: event.target.value })
+
   render() {
+    const {messages, messageInput} = this.state
     return (
       <div>
         <div className="chat">
-          <Message />
+        { messages.map((message) => (<Message text={ message } />)) }
         </div>
-        <input className="input-message"/>
+        <input
+          className="input-message"
+          type="text"
+          onKeyPress={ this.sendMessageOnEnter }
+          onChange={ this.changeInputMessage }
+          value={ messageInput }
+        />
       </div>
     )
   }
--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,11 +1,9 @@
+import React from 'react';
 import './Message.css';
 
-const Message = () => {
-  return (
-    <div className="message-list">
-      <div className="messages"></div>
-    </div>
-  )
+export default class Message extends React.Component {
+  render() {
+    return this.props.text &&
+      <div className="message-list"><span className="message"> {this.props.text} </span></div>
+  }
 }
-
-export default Message ;
